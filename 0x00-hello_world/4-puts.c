d5702e5ab4810fd8fcc3be315d93fcc9d2a7ecb8
wisdomnkanta <linkwithdiamond@example.com>
2022-10-15 03:28:33 -0700

    low level programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+nclude <stdio.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	char str1[] = "\"Programming is like building a multilingual puzzle";
+
+
+			puts(str1);
+
+			return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	char str1[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+
+			printf("%s", str1);
+
+				return (0);
+
+}
