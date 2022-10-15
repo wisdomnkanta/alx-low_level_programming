d0942d9baf9620530b8a830f72e7eb75ab4a3525
wisdomnkanta <linkwithdiamond@example.com>
2022-10-15 04:12:19 -0700

    move on

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-		char str1[] = "\"Programming is like building a multilingual puzzle";
-
-
-
-			puts(str1);
-
-				return (0);
-
+	char str1[] = "\"Programming is like building a multilingual puzzle";
+	puts (str1);
+	return (0);
 }
