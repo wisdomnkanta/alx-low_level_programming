6133fc317f21a27067d86594189870d0592d5a17
wisdomnkanta <linkwithdiamond@example.com>
2022-10-16 06:15:58 -0700

    Asa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	char str1[] = "\"Programming is like building a multilingual puzzle";
+	char str1[] = "\"programming is like building a multilingual puzzle";
+
 	puts(str1);
 	return (0);
 }
