f3d99c1c2f1e7e4c941db87a6121f9c2e332c902
wisdomnkanta <linkwithdiamond@example.com>
2022-10-18 15:59:15 -0700

    snoop

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,16 +8,16 @@ void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0; hour <=23; hour++)
+	for(hour = 0; hour <=23; hour++)
 	{
-		for (minute = 0; minute <= 59; minute++)
+		for (minute = 0; minute <=59; minute++)
 		{
 			_putchar('0' + (hour / 10));
 			_putchar('0' + (hour % 10));
 			_putchar(':');
 			_putchar('0' + (minute / 10));
 			_putchar('0' + (minute % 10));
-			_putchar('\n');
+			_purchar('\n');
 		}
 	}
 }
