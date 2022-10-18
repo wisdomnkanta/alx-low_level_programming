88581355ae3d53d9ab30cf977a73705c23f8a8ef
wisdomnkanta <linkwithdiamond@example.com>
2022-10-18 16:57:29 -0700

    stress

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,19 +13,19 @@ void times_table(void)
 		for (b = 0; b < 10; b++)
 		{
 			c = a * b;
-			if (b ==0)
+			if (b == 0)
 			{
 				_putchar(c + '0');
 			}
 
-			if (c < 10 && b ! = 0)
+			if (c < 10 && b != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(c + '0');
 			}
-			else if (c >=10)
+			else if (c >= 10)
 			{
 				_putchar(',');
 				-putchar(' ');
