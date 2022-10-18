d584a45f9f54a76e437d01c27e583266c84ae862
wisdomnkanta <linkwithdiamond@example.com>
2022-10-18 16:11:15 -0700

    book

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * jack_bauer - function print every hour and minutes
+ * jack_bauer - fucntion print every hour and minutes
  * Return: 0 successfully
  */
-void jack_bauer(void)
+void jack-bauer(void)
 {
 	int hour, minute;
 
-	for(hour = 0; hour <=23; hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (minute = 0; minute <=59; minute++)
+		for (minute = 0; minute <= 59; minute++)
 		{
 			_putchar('0' + (hour / 10));
 			_putchar('0' + (hour % 10));
 			_putchar(':');
 			_putchar('0' + (minute / 10));
 			_putchar('0' + (minute % 10));
-			_purchar('\n');
+			_putchar('\n');
 		}
 	}
 }
