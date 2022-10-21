74f42d243122b861f78e0e77cc8def0c74f85115
wisdomnkanta <linkwithdiamond@example.com>
2022-10-20 17:08:37 -0700

    edit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n");
+	printf("infinte loop incoming :(\n");
 
 	i = 0;
 
@@ -18,7 +18,7 @@ int main(void)
 	/*putchar(i);*/
 	/*}*/
 
-	printf("infinite loop avoided! \\o/\n");
+	printf("infinte loop avoided! \\o/\n");
 
 	return (0);
 }
