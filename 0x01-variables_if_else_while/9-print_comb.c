0a8280e65d850df4f020c7b73da4dfd09c23b6ae
wisdomnkanta <linkwithdiamond@example.com>
2022-10-21 23:21:16 -0700

    total

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single digit numbers
+ * Return: Alwaays 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
