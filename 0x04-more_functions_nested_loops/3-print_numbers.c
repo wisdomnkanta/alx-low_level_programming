d4c08f2fce8f8a8eb2e5284a83a5d5a141c7a67c
wisdomnkanta <linkwithdiamond@example.com>
2022-10-26 15:20:28 -0700

    prog

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include 'main.h"
+
+/**
+ * print_numbers - prints number 0 to 9
+ * Return: 0 successfully
+ */
+void print_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbr from 0 - 9
+ * except 2 and 9
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= 9'; n++)
+	{
+		if (n != '2' && n != '4')
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
